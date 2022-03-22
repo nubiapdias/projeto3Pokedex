2963dafe2b8f8c3796b9685a90db4457ae86f955
Bia Dias <93796266+nubiapdias@users.noreply.github.com>
2022-03-22 18:52:30 -0300

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded());
 
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.listen(port, (req, res) => {
   console.log(`Estou rodando na porta ${port}`);
